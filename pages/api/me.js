775dfeb2326f0ee612a0b2089ea3174cb485929d
YTG2G3 <scratchlover05@gmail.com>
2022-09-04 00:10:51 -1000

    hard coded for submission

--- pages/api/me.js
+++ pages/api/me.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     if (resp.rowCount === 0) {
         let name = nanoid();
         await pg.query("INSERT INTO user (email, name, ums_front, ums_back, os_front, os_back, ut_front, ut_back, ums, os, ut) VALUES($1, $2, 'map', 'state', 'state', 'capital', 'territory', 'region', $3, $4, $5)", [session.user.email, name, (new Array(193).fill("1")).join(""), (new Array(2).fill("1")).join(""), (new Array(5).fill("1")).join("")]);
-        res.status(200).json({ email: session.user.email, name, ums_front: 'map', ums_back: 'state', os_front: 'state', os_back: 'capital', ut_front: 'territory', ut_back: 'region', ums: (new Array(193).fill("1")).join(""), os: (new Array(2).fill("1")).join(""), ut: (new Array(5).fill("1")).join("") });
+        res.status(200).json({ email: session.user.email, name, ums_front: 'map', ums_back: 'state', os_front: 'state', os_back: 'capital', ut_front: 'region', ut_back: 'territory', ums: (new Array(193).fill("1")).join(""), os: (new Array(2).fill("1")).join(""), ut: (new Array(5).fill("1")).join("") });
     }
     else res.status(200).json(resp.rows[0]);
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -12,6 +12,7 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
     let [settings, setSettings] = useState(undefined);
     let [flashcards, setFlashcards] = useState([]);
     let [fcIndex, setFcIndex] = useState(0);
+    let [overlay, setOverlay] = useState(false);
 
     useEffect(() => loadSettings(), []);
 
@@ -42,7 +43,7 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
     const loadSettings = () => {
         if (session) axios.get("/api/me").then(resp => decodeSettings(resp.data));
         else if (localStorage.getItem("settings")) decodeSettings(JSON.parse(localStorage.getItem("settings")));
-        else decodeSettings({ ums_front: 'map', ums_back: 'state', os_front: 'state', os_back: 'capital', ut_front: 'territory', ut_back: 'region', ums: (new Array(193).fill("1")).join(""), os: (new Array(2).fill("1")).join(""), ut: (new Array(5).fill("1")).join("") });
+        else decodeSettings({ ums_front: 'map', ums_back: 'state', os_front: 'state', os_back: 'capital', ut_front: 'region', ut_back: 'territory', ums: (new Array(193).fill("1")).join(""), os: (new Array(2).fill("1")).join(""), ut: (new Array(5).fill("1")).join("") });
     }
 
     const decodeSettings = (s) => {
@@ -59,42 +60,62 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
         setFlashcards(tmp);
     }
 
+    function FC() {
+        let k = flashcards[fcIndex].flipped
+            ? settings[flashcards[fcIndex].type + "_front"]
+            : settings[flashcards[fcIndex].type + "_back"];
+
+        if (k === "map") return <Image src={flashcards[fcIndex].map} width={400} height={400} />
+        else return <h1 style={{ color: flashcards[fcIndex].flipped ? "white" : "black" }}>{flashcards[fcIndex][k]}</h1>
+    }
+
     return (
-        <div className={styles.page}>
-            <div className={styles.nav}>
-                {session
-                    ? <button title="Log Out" onClick={() => signOut()}><Image src="/person-circle.svg" width={40} height={40} /></button>
-                    : <button title="Log In" onClick={() => signIn("google")}><Image src="/google.svg" width={40} height={40} /></button>
-                }
+        <>
+            <div className={styles.settings} style={{ display: overlay ? "block" : "none" }} onClick={() => setOverlay(false)}>
+                <div className={styles.wrapper}>
 
-                <button title="Settings"><Image src="/gear-fill.svg" width={40} height={40} /></button>
+                </div>
             </div>
 
-            <main className={styles.main}>
-                <div className={styles.marg}>
-                    <button onClick={() => setFcIndex(fcIndex - 1)}>
-                        <Image src="/caret-left.svg" width={80} height={80} />
-                    </button>
+            <div className={styles.page}>
+                <div className={styles.nav}>
+                    {session
+                        ? <button title="Log Out" onClick={() => signOut()}><Image src="/person-circle.svg" width={40} height={40} /></button>
+                        : <button title="Log In" onClick={() => signIn("google")}><Image src="/google.svg" width={40} height={40} /></button>
+                    }
+
+                    <button title="Settings" onClick={() => setOverlay(true)}><Image src="/gear-fill.svg" width={40} height={40} /></button>
                 </div>
 
-                <div className={styles.flashcard} onClick={flipCard}>
-                    <h1 style={{ color: flashcards[fcIndex].flipped ? "white" : "black" }}>{flashcards.length > 0 ? flashcards[fcIndex].flipped
-                        ? flashcards[fcIndex][settings[flashcards[fcIndex].type + "_front"]]
-                        : flashcards[fcIndex][settings[flashcards[fcIndex].type + "_back"]]
-                        : "Loading..."}</h1>
+                <main className={styles.main}>
+                    <div className={styles.marg}>
+                        {fcIndex > 0 ? <button onClick={() => setFcIndex(fcIndex - 1)}>
+                            <Image src="/caret-left.svg" width={80} height={80} />
+                        </button> : undefined}
+                    </div>
+
+                    <div className={styles.flashcard} onClick={flipCard}>
+                        {flashcards.length > 0 ?
+                            <>
+                                <FC />
+
+                                <p style={{ color: flashcards[fcIndex].flipped ? "white" : "black" }}>{flashcards[fcIndex].flipped ? settings[flashcards[fcIndex].type + "_front"] : settings[flashcards[fcIndex].type + "_back"]} of {flashcards[fcIndex].type === "ums" ? "un member states" : flashcards[fcIndex].type === "os" ? "observer states" : "us territory"}</p>
+                            </>
+                            : undefined}
+                    </div>
+
+                    <div className={styles.marg}>
+                        {fcIndex < flashcards.length - 1 ? <button onClick={() => setFcIndex(fcIndex + 1)}>
+                            <Image src="/caret-right.svg" width={80} height={80} />
+                        </button> : undefined}
+                    </div>
+                </main>
+
+                <div className={styles.menu}>
+                    <button title="Shuffle" onClick={() => shuffle(settings)}><Image src="/shuffle.svg" width={40} height={40} /></button>
                 </div>
-
-                <div className={styles.marg}>
-                    <button onClick={() => setFcIndex(fcIndex + 1)}>
-                        <Image src="/caret-right.svg" width={80} height={80} />
-                    </button>
-                </div>
-            </main>
-
-            <div>
-
             </div>
-        </div>
+        </>
     );
 }
 
