4b9c82cce8ddc8c070e737c1260774d648ed7027
YTG2G3 <scratchlover05@gmail.com>
2022-09-03 22:40:19 -1000

    color changed

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,9 +10,34 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
     let { data: session } = useSession();
 
     let [settings, setSettings] = useState(undefined);
-    let [name, setName] = useState(undefined);
+    let [flashcards, setFlashcards] = useState([]);
+    let [fcIndex, setFcIndex] = useState(0);
 
     useEffect(() => loadSettings(), []);
+
+    const shuffleArr = (array) => {
+        let currentIndex = array.length, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex--;
+
+            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+        }
+
+        return array;
+    }
+
+    const shuffle = (settings) => {
+        let { ums, os, ut } = settings;
+        let tmp = [];
+
+        tmp = tmp.concat(un_member_state.filter((v, i) => ums.charAt(i) === '1').map(v => ({ ...v, type: "ums" })));
+        tmp = tmp.concat(observer_state.filter((v, i) => os.charAt(i) === '1').map(v => ({ ...v, type: "os" })));
+        tmp = tmp.concat(us_territory.filter((v, i) => ut.charAt(i) === '1').map(v => ({ ...v, type: "ut" })));
+
+        setFlashcards(shuffleArr(tmp.map(v => ({ ...v, flipped: false }))));
+    }
 
     const loadSettings = () => {
         if (session) axios.get("/api/me").then(resp => decodeSettings(resp.data));
@@ -21,28 +46,50 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
     }
 
     const decodeSettings = (s) => {
-        if (s.name) setName(s.name);
-
         let { ums_front, ums_back, os_front, os_back, ut_front, ut_back, ums, os, ut } = s;
 
         setSettings({ ums_front, ums_back, os_front, os_back, ut_front, ut_back, ums, os, ut });
-        localStorage.setItem("settings", JSON.stringify({ ums_front, ums_back, os_front, os_back, ut_front, ut_back, ums, os, ut }))
+        localStorage.setItem("settings", JSON.stringify({ ums_front, ums_back, os_front, os_back, ut_front, ut_back, ums, os, ut }));
+        shuffle({ ums, os, ut });
+    }
+
+    const flipCard = () => {
+        let tmp = [...flashcards];
+        tmp[fcIndex].flipped = !tmp[fcIndex].flipped;
+        setFlashcards(tmp);
     }
 
     return (
         <div className={styles.page}>
             <div className={styles.nav}>
                 {session
-                    ? <button title="Log Out" onClick={() => signOut()}><Image src="/person-circle.svg" width={40} height={40}></Image></button>
-                    : <button title="Log In" onClick={() => signIn("google")}><Image src="/google.svg" width={40} height={40}></Image></button>
+                    ? <button title="Log Out" onClick={() => signOut()}><Image src="/person-circle.svg" width={40} height={40} /></button>
+                    : <button title="Log In" onClick={() => signIn("google")}><Image src="/google.svg" width={40} height={40} /></button>
                 }
 
-                <button title="Settings"><Image src="/gear-fill.svg" width={40} height={40}></Image></button>
+                <button title="Settings"><Image src="/gear-fill.svg" width={40} height={40} /></button>
             </div>
 
-            <div className={styles.flashcard}>
-                <button onClick={loadSettings}>hello world</button>
-            </div>
+            <main className={styles.main}>
+                <div className={styles.marg}>
+                    <button onClick={() => setFcIndex(fcIndex - 1)}>
+                        <Image src="/caret-left.svg" width={80} height={80} />
+                    </button>
+                </div>
+
+                <div className={styles.flashcard} onClick={flipCard}>
+                    <h1 style={{ color: flashcards[fcIndex].flipped ? "white" : "black" }}>{flashcards.length > 0 ? flashcards[fcIndex].flipped
+                        ? flashcards[fcIndex][settings[flashcards[fcIndex].type + "_front"]]
+                        : flashcards[fcIndex][settings[flashcards[fcIndex].type + "_back"]]
+                        : "Loading..."}</h1>
+                </div>
+
+                <div className={styles.marg}>
+                    <button onClick={() => setFcIndex(fcIndex + 1)}>
+                        <Image src="/caret-right.svg" width={80} height={80} />
+                    </button>
+                </div>
+            </main>
 
             <div>
 
@@ -52,7 +99,6 @@ export default function Flashcard({ un_member_state, observer_state, us_territor
 }
 
 export async function getStaticProps() {
-    return { props: { un_member_state: [], observer_state: [], us_territory: [] }, revalidate: 3600 }
     let un_member_state = (await pg.query("SELECT * FROM un_member_state")).rows;
     let observer_state = (await pg.query("SELECT * FROM observer_state")).rows;
     let us_territory = (await pg.query("SELECT * FROM us_territory")).rows;
